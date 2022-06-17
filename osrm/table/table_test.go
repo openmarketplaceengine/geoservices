6b20d3f486fa933e98b13570cfa23c0f5e0b9990
Denis Kuchugurov <kepkap.dev@gmail.com>
2022-06-17 18:29:09 +0300

    feat(osrm): table usage

--- osrm/table/table_test.go
+++ osrm/table/table_test.go
@@ -30,3 +30,31 @@ func TestTable(t *testing.T) {
 	require.Greater(t, response.Distances[0][1], float64(0))
 	require.Greater(t, response.Durations[0][1], float64(0))
 }
+
+func TestTableMultipleOrigins(t *testing.T) {
+	request := Request{
+		Coordinates: []osrm.Coordinate{
+			{40.751753, -73.980052},
+			{40.794156, -73.962662},
+			{40.785965, -73.952794},
+		},
+		Origins:      []int{0, 1},
+		Destinations: []int{0, 1, 2},
+		Annotations:  DurationDistance,
+	}
+
+	response, err := Table(&http.Client{}, request)
+	require.NoError(t, err)
+	require.Equal(t, "Ok", response.Code)
+
+	require.Len(t, response.Destinations, 3)
+	require.Len(t, response.Sources, 2)
+
+	require.Len(t, response.Distances, 2)
+	require.Len(t, response.Distances[0], 3)
+	require.Len(t, response.Distances[1], 3)
+	require.Len(t, response.Durations, 2)
+	require.Len(t, response.Durations[0], 3)
+	require.Len(t, response.Durations[1], 3)
+
+}
