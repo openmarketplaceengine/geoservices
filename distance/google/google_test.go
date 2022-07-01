6753ee60e72aa7794c0f1053c24dff5bd37f2eef
Denis Kuchugurov <kepkap.dev@gmail.com>
2022-07-01 10:03:59 +0300

    feat(google): get matrix for multiple origins test added (#12)
    * feat(google): get matrix for multiple origins test added
    
    * feat(google): get matrix for multiple origins test added

--- distance/google/google_test.go
+++ distance/google/google_test.go
@@ -25,6 +25,10 @@ func TestGetMatrix(t *testing.T) {
 	t.Run("testGetMatrixFromPlaces", func(t *testing.T) {
 		testGetMatrixFromPlaces(t)
 	})
+
+	t.Run("testGetMatrixMultipleOrigins", func(t *testing.T) {
+		testGetMatrixMultipleOrigins(t)
+	})
 }
 
 func testGetMatrix(t *testing.T) {
@@ -68,4 +72,49 @@ func testGetMatrixFromPlaces(t *testing.T) {
 	require.Greater(t, matrix.Rows[0].Elements[0].Distance, 0)
 	require.Equal(t, matrix.OriginAddresses[0], "96 St, New York, NY 10025, USA")
 	require.Equal(t, matrix.DestinationAddresses[0], "59 St-Columbus Circle, Columbus Cir, New York, NY 10023, USA")
+}
+
+func testGetMatrixMultipleOrigins(t *testing.T) {
+	driver := geoservices.LatLng{
+		Lat: 40.788088,
+		Lng: -73.970988,
+	}
+	pickup1 := geoservices.LatLng{
+		Lat: 40.791680,
+		Lng: -73.965011,
+	}
+	destination1 := geoservices.LatLng{
+		Lat: 40.768660,
+		Lng: -73.981454,
+	}
+	pickup2 := geoservices.LatLng{
+		Lat: 40.791677,
+		Lng: -73.965011,
+	}
+	destination2 := geoservices.LatLng{
+		Lat: 40.773261,
+		Lng: -73.988607,
+	}
+
+	ctx := context.Background()
+	client, err := maps.NewClient(maps.WithAPIKey(apiKey))
+	require.NoError(t, err)
+	matrix, err := Matrix(ctx, client, distance.MatrixPointsInput{
+		Origins:      []geoservices.LatLng{driver, pickup1, pickup2},
+		Destinations: []geoservices.LatLng{destination1, destination2},
+	})
+	require.NoError(t, err)
+	require.Len(t, matrix.OriginAddresses, 3)
+	require.Equal(t, matrix.OriginAddresses[0], "586 Columbus Ave, New York, NY 10024, USA")
+	require.Equal(t, matrix.OriginAddresses[1], "96 St, New York, NY 10025, USA")
+	require.Len(t, matrix.DestinationAddresses, 2)
+	require.Equal(t, matrix.DestinationAddresses[0], "59 St-Columbus Circle, Columbus Cir, New York, NY 10023, USA")
+	require.Len(t, matrix.Rows, 3)
+	require.Len(t, matrix.Rows[0].Elements, 2)
+	require.Greater(t, matrix.Rows[0].Elements[0].Duration, time.Duration(0))
+	require.Greater(t, matrix.Rows[0].Elements[0].Distance, 0)
+
+	require.Len(t, matrix.Rows[1].Elements, 2)
+	require.Greater(t, matrix.Rows[1].Elements[0].Duration, time.Duration(0))
+	require.Greater(t, matrix.Rows[1].Elements[0].Distance, 0)
 }
