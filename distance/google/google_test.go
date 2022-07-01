35fd4cf014c49c078590b97ad0f99b404729b2a0
Denis Kuchugurov <kepkap.dev@gmail.com>
2022-07-01 07:52:22 +0300

    feat(google): get matrix for multiple origins test added

--- distance/google/google_test.go
+++ distance/google/google_test.go
@@ -79,34 +79,42 @@ func testGetMatrixMultipleOrigins(t *testing.T) {
 		Lat: 40.788088,
 		Lng: -73.970988,
 	}
-	pickup := geoservices.LatLng{
-		Lat: 40.791680675548136,
-		Lng: -73.9650115649754,
+	pickup1 := geoservices.LatLng{
+		Lat: 40.791680,
+		Lng: -73.965011,
 	}
-	destination := geoservices.LatLng{
-		Lat: 40.76866089218841,
-		Lng: -73.98145413365043,
+	destination1 := geoservices.LatLng{
+		Lat: 40.768660,
+		Lng: -73.981454,
+	}
+	pickup2 := geoservices.LatLng{
+		Lat: 40.791677,
+		Lng: -73.965011,
+	}
+	destination2 := geoservices.LatLng{
+		Lat: 40.773261,
+		Lng: -73.988607,
 	}
 
 	ctx := context.Background()
 	client, err := maps.NewClient(maps.WithAPIKey(apiKey))
 	require.NoError(t, err)
 	matrix, err := Matrix(ctx, client, distance.MatrixPointsInput{
-		Origins:      []geoservices.LatLng{driver, pickup},
-		Destinations: []geoservices.LatLng{destination},
+		Origins:      []geoservices.LatLng{driver, pickup1, pickup2},
+		Destinations: []geoservices.LatLng{destination1, destination2},
 	})
 	require.NoError(t, err)
-	require.Len(t, matrix.OriginAddresses, 2)
+	require.Len(t, matrix.OriginAddresses, 3)
 	require.Equal(t, matrix.OriginAddresses[0], "586 Columbus Ave, New York, NY 10024, USA")
 	require.Equal(t, matrix.OriginAddresses[1], "96 St, New York, NY 10025, USA")
-	require.Len(t, matrix.DestinationAddresses, 1)
+	require.Len(t, matrix.DestinationAddresses, 2)
 	require.Equal(t, matrix.DestinationAddresses[0], "59 St-Columbus Circle, Columbus Cir, New York, NY 10023, USA")
-	require.Len(t, matrix.Rows, 2)
-	require.Len(t, matrix.Rows[0].Elements, 1)
+	require.Len(t, matrix.Rows, 3)
+	require.Len(t, matrix.Rows[0].Elements, 2)
 	require.Greater(t, matrix.Rows[0].Elements[0].Duration, time.Duration(0))
 	require.Greater(t, matrix.Rows[0].Elements[0].Distance, 0)
 
-	require.Len(t, matrix.Rows[1].Elements, 1)
+	require.Len(t, matrix.Rows[1].Elements, 2)
 	require.Greater(t, matrix.Rows[1].Elements[0].Duration, time.Duration(0))
 	require.Greater(t, matrix.Rows[1].Elements[0].Distance, 0)
 }
